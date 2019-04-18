5f68c989e9ee400417abdd917127ce9e7bd7d410
flipthemouse <49499331+flipthemouse@users.noreply.github.com>
2019-04-18 09:11:44 +0200

    Add files via upload

---
+++ Disable-UAC.ps1
@@ -0,0 +1,25 @@
+﻿  <#
+        .SYNOPSIS
+            Disable UAC though powershell
+
+        .DESCRIPTION
+            Disable UAC though powershell
+#>
+
+$osversion = (Get-CimInstance Win32_OperatingSystem).Version
+$version = $osversion.split(".")[0]
+
+if ($version -eq 10) {
+	Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value "0"
+} ElseIf ($Version -eq 6) {
+	$sub = $version.split(".")[1]
+    if ($sub -eq 1 -or $sub -eq 0) {
+		Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Value "0"
+    } Else {
+		Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value "0"
+    }
+} ElseIf ($Version -eq 5) {
+	Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Value "0"
+} Else {
+	Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value "0"
+}
